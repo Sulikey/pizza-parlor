86e3116a7882c0697642e152941de66bc4982675
sulikey <anahi.sulikey@gmail.com>
2022-11-04 12:19:33 -0700

    add constructors and prototype WIP

---
+++ README.md
@@ -0,0 +1,41 @@
+# _Mr.Robogers-Neighborhood_
+
+#### By _**Anahi Ruiz Robles**_
+
+#### _Love funny robots well this webpage will beep boop once you enter a couple digits!_
+
+## Technologies Used
+
+* _HTML_
+* _CSS_
+* _JS_
+
+## Description
+
+_This webpage contains a silly robot that will beep boop once you enter any number._
+
+## Setup/Installation Requirements
+
+* _Clone this repository to your desktop._
+* _Navigate to the top level of the directory._
+* _Open index.html in your browser._
+
+## Known Bugs
+
+* _No known bugs_
+
+## License
+
+_[MIT](https://choosealicense.com/licenses/mit/)_
+
+Copyright (c) _11/04/2022_ _Anahi Ruiz Robles_
+
+```
+Describe: construtor Order()
+Test:should make pizzas into empty object
+Code: 
+ function Order() { this.pizzas = {}}
+Expected Output: this.pizzas is now empty object
+
+
+```
---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+function Order() {
+  this.pizzas = {}
+} 
+//{} is shorthand for creating an empty object.//
+
+function Pizza (name, size, toppings = [], price) {
+  this.name = name;
+  this.size = size;
+  this.toppings = toppings;
+  this.pizzaPrice = price;
+} 
+//[] is shorthand for creating an empty array.//
+
+Order.prototype.addPizza = function(pizza) {
+  this.pizzas[this.pizzas.name] = pizza;
+}
+
+Order.prototype.locatePizza = function(name) {
+  if(this.pizzas[name] !== undefined) {
+    return this.pizzas[name];
+  }
+  return false;
+} //!== checks whether its two operands are not equal//
+
+
+Pizza.prototype.price = function() {
+  if (this.size === 'large' && this.toppings.includes('salami') || this.size === 'large' && this.toppings.includes('beef') || this.size === 'large' && this.toppings.includes('mushrooms')) {
+    let price = '$40';
+    return price;
+  } else if (this.size === 'medium' && this.toppings.includes('salami') || this.size === 'medium' && this.toppings.includes('beef') || this.size === 'medium'&& this.toppings.includes('mushrooms')) {
+    let price = '$35';
+    return price;
+  } else if (this.size === 'small' && this.toppings.includes('salami') || this.toppings.includes('salami') || this.toppings.includes('beef') || this.toppings.includes('mushrooms')) {
+    let price = '$30';
+    return price;
+  } else if (this.size === 'large') {
+    let price = '$35';
+    return price;
+  } else if(this.size === 'medium') {
+    let cost = '$30';
+    return price;
+  } else if (this.size === "small") {
+    let price = '$25';
+    return price;
+  }
+}
